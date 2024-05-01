0f9bda888d09c00333b8ca841d32fbb0ef32f35f
Skovkid <48293039+Skovkid@users.noreply.github.com>
2024-05-01 20:25:54 +0200

    Add files via upload

--- Descriptive_statistics.R
+++ Descriptive_statistics.R
@@ -1,20 +1,34 @@
 # Visualize missing data over time
-library(ggplot2)
+
+library(dplyr)
 library(tidyr)
+library(purrr)
+library(knitr)
 
-result_latin_america %>%
-  gather(key = "variable", value = "value", -iso2c, -year) %>%
-  mutate(missing = is.na(value)) %>%
-  ggplot(aes(x = year, y = iso2c, fill = missing)) +
-  geom_tile() +
-  scale_fill_manual(values = c("reported" = "blue", "missing" = "grey")) +
-  labs(fill = "Data Status",
-       x = "Year",
-       y = "Country ISO2 Code") +
-  theme_minimal()
 
-# Analyze patterns of missing data
-library(naniar)
-result_latin_america %>%
-  is.na() %>%
-  colSums()
+
+
+
+observations_summary <- Reg_w_NA_df_No_TM %>%
+  summarise(
+    GDP_Growth_Cap_Observations = sum(!is.na(GDP_Growth_Cap)),
+    GDP_Per_Capita_Growth_Observations = sum(!is.na(GDP_Per_Capita_Growth)),
+    Prop_Women_Observations = sum(!is.na(Prop_Women)),
+    Trade_Observations = sum(!is.na(Trade)),
+    Capital_formation_Observations = sum(!is.na(Capital_formation)),
+    WBL_ind_Observations = sum(!is.na(WBL_ind))
+  )
+
+# View the summary of observations
+print(observations_summary)
+
+
+
+# Create the scatter plot
+ggplot(Reg_w_NA_df_No_TM, aes(x = Prop_Women, y = GDP_Per_Capita_Growth)) +
+  geom_point(color = "blue", size = 2) +
+  ggtitle("Scatter Plot of GDP Per Capita Growth vs. Proportion Women in Parliament") +
+  xlab("Proportion Women in Parliament") +
+  ylab("GDP Per Capita Growth") +
+  geom_hline(yintercept = 0, color = "red", size = 1.5) +  # Add horizontal line at y = 0
+  theme_minimal()  # Applies a minimalistic theme
